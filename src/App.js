68c780aab8b0af49249b8d779e5cde91e9b0f324
Marcos Alvim <alvim.marcos@gmail.com>
2020-03-26 01:44:38 -0300

    front

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Mr. Pin ...só no anel.. é!!
         </p>
         <a
           className="App-link"
