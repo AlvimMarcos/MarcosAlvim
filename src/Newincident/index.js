801fe08fa7d4da6bc6ab13afc4e88951f063c943
Marcos Alvim <alvim.marcos@gmail.com>
2020-04-29 16:03:54 -0300

    final frontend

--- src/Newincident/index.js
+++ src/Newincident/index.js
@@ -1,13 +1,50 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link, useHistory} from 'react-router-dom';
 import{FiArrowLeft} from 'react-icons/fi';
 
+import api from '../services/api';
 
 import './styles.css';
 import logoImg from '../assets/logo.svg';
 
 
 export default function NewIncident() {
+    const[title, setTitle] = useState('');
+    const[description, setDescription] = useState('');
+    const[value, setValue] = useState('');
+    
+    const history = useHistory();
+    const ongId = localStorage.getItem('ongId');
+
+
+    async function handleNewIncident(e){
+        e.preventDefault();
+
+        const data={
+            title,
+            description,
+            value,
+        };
+
+        try{
+            await api.post('incidents', data, {
+                headers: {
+                    Authorization:ongId,
+                }
+            } )
+
+        history.push('/profile');    
+
+
+        }catch(err){
+            alert('Error. Try to again.');
+        }
+
+    }
+
+
+
+
     return (
         <div className="new-incident-container">
             <div className="content">
@@ -22,12 +59,22 @@ export default function NewIncident() {
                     Back to the HOME Page
                 </Link>
                 </section>
-                <form>
-                    <input placeholder="Case title"/>
-                    <textarea placeholder="Description"/>
-                    
-                    
-                     <input placeholder="Value in US$"/>
+                <form onSubmit={handleNewIncident}>
+                    <input 
+                        placeholder="Case title"
+                        value={title}
+                        onChange={e => setTitle(e.target.value) }
+                    />
+                    <textarea 
+                        placeholder="Description"
+                        value={description}
+                        onChange={e => setDescription(e.target.value) }
+                    />
+                     <input 
+                        placeholder="Value in US$"
+                        value={value}
+                        onChange={e => setValue(e.target.value) }
+                    />
                      
 
                     <button className="button" type="submit">INSERT</button>
--- src/Profile/index.js
+++ src/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from "react-icons/fi";
 
 
@@ -11,6 +11,8 @@ import logoImg from '../assets/logo.svg';
 
 export default function Profile() {
     const [incidents, setIncidents] = useState([]);
+
+    const history = useHistory();
 
 
     const ongId = localStorage.getItem('ongId');
@@ -26,36 +28,49 @@ export default function Profile() {
         })
     }, [ongId]);
 
-    async function handleDeleteIncident (id) {
-        try{    
+    async function handleDeleteIncident(id) {
+        try {
             await api.delete(`incidents/${id}`, {
-                    headers:{
-                        Authorization:ongId,
-                    }
+                headers: {
+                    Authorization: ongId,
+                }
             });
-        }catch (err){
-            alert('Deleting Error, try again');
+
+           
+            setIncidents(incidents.filter(incident => incident.id !== id));
+        } catch (err) {
+            alert('Deleting Error, try again.');
+        }
+    }
+
+
+
+     function handleLogOut() {
+            localStorage.clear();
+
+            history.push('/');
+
         }
 
-    }
+    
     return (
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="Be the Hero" />
                 <span>Welcome, {ongName}</span>
                 <Link className="button" to="/incidents/new"> Insert new case</Link>
-                <button type="button" />
+                <button onClick={handleLogOut} type="button" />
                 <FiPower size={18} color="E020041" />
             </header>
             <h1> Registered Cases </h1>
 
-            
 
-            {incidents.length ===0 && <h1>Loading 3...</h1> }
-            
 
-            
-             {/* <h1 className={incidents.length===0 ? 'display:none;' : ''}> Loading... </h1>            */}
+            {incidents.length === 0 && <h1>Loading 3...</h1>}
+
+
+
+            {/* <h1 className={incidents.length===0 ? 'display:none;' : ''}> Loading... </h1>            */}
 
 
 
@@ -71,9 +86,9 @@ export default function Profile() {
                         <p>{incident.description}</p>
 
                         <strong>VALUE/PRICE:</strong>
-                        <p>{Intl.NumberFormat('en-US', {style:'currency',currency:'USD'    }).format(incident.value )}</p>
+                        <p>{Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(incident.value)}</p>
 
-                        <button onClick={ () => handleDeleteIncident(incident.id)} type="button">
+                        <button onClick={() => handleDeleteIncident(incident.id)} type="button">
                             <FiTrash2 size={20} color="a8a8b3" />
                         </button>
                     </li>
@@ -89,4 +104,4 @@ export default function Profile() {
             </ul>
         </div>
     );
-}
+ }
